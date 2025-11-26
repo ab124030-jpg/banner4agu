3fd9379effee74698d0a52a3d108deac31e3498e
ab124030-jpg <ab124030@aoyama.jp>
2025-11-26 18:42:00 +0900

    Refactor bannercmd.sh to remove banner output
    Removed banner echo statements and added sample code.

--- bannercmd.sh
+++ bannercmd.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-echo "****************************************"
-echo "* Aoyama Gakuin University Banner Tool *"
-echo "****************************************"
+@@ -1 +1 @@
+This is a sample short code for my students.
+This is [a sample short code](https://github.com/mapconcierge/banner4agu/blob/main/bannercmd.sh) for my students.
